2352501d96686ccfbe5199916973bfcd94b266a1
ruangls <147521624+ruangls@users.noreply.github.com>
2023-11-14 21:01:12 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
-
+document.querySelector("html").addEventListener("click", () => {
+    alert("别戳我，我怕疼。");
+  });
   let myImage = document.querySelector("img");
 
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/jyu.jpg") {
+  if (mySrc === "images/111.jpg") {
     myImage.setAttribute("src", "images/112.gif");
   } else {
     myImage.setAttribute("src", "images/113.jpg");
